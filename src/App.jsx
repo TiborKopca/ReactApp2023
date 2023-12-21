945da9162ec0c86f414a78296ac89df1a5ecf387
TiborKopca <tibor.kopca@gmail.com>
2023-12-21 19:04:19 +0100

    Created Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg' */
 import './App.css'
+import Header from './Header.jsx'
 
 function App() {
-
-
-
-
+  return(
+    <Header></Header>
+    
+  );
 
 
   /* Default
---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+    return(
+        <header>
+            <h1>Website in React!</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">My work</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+export default Header
