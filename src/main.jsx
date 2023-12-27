7e8b1a038430d670c1bb8b4f3689a4586bd25355
TiborKopca <tibor.kopca@gmail.com>
2023-12-27 17:02:34 +0100

    reorganizizing folders & media querries

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//ReactDOM.render() is deprecated
+//Obtain the reference of the 'div' called 'root' to generate HTML elements
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
