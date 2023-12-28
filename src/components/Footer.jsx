6387e2007a9956d9aa2e9c70eb2164b64fe36c0c
TiborKopca <tibor.kopca@gmail.com>
2023-12-28 17:30:57 +0100

    Footer inline styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,12 @@
 function Footer(){
+    const styles = {
+        height: '40px',
+        padding: '10px',
+        textAlign: 'center',
+        backgroundColor: '#f3782d99'
+    }
     return(
-        <footer>
+        <footer style={styles}>
             <p>&copy; {new Date().getFullYear()} Tibor Kopca</p>
         </footer>
     );
